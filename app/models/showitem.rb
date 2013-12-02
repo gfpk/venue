33b67366d6d2bd2ed9eef10c75460371119477d3
gfpk <nci@nci-VirtualBox.(none)>
2013-12-02 13:02:00 +0000

    upload done, protected tree needed

--- app/models/showitem.rb
+++ app/models/showitem.rb
@@ -4,5 +4,6 @@ class Showitem < ActiveRecord::Base
   has_many :tickets , :through =>:showtimes
   attr_accessible :description, :featured, :image, :name, :showtimes_attributes
   accepts_nested_attributes_for :showtimes, allow_destroy: true
+  mount_uploader :image, ImageUploader
 
 end
