558c30224ac5931d463db539b054a119b354b7f9
gfpk <nci@nci-VirtualBox.(none)>
2013-12-03 17:07:37 +0000

    devise installed, vuiews for user needed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Venue::Application.routes.draw do
+  devise_for :users
+
   resources :line_items
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131125204013) do
+ActiveRecord::Schema.define(:version => 20131203170127) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -58,5 +58,23 @@ ActiveRecord::Schema.define(:version => 20131125204013) do
   end
 
   add_index "tickets", ["showtime_id"], :name => "index_tickets_on_showtime_id"
+
+  create_table "users", :force => true do |t|
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
+  end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
